4fa3b205f03253ad167c0dd06cd8dfa23ed09909
6604062630269 <152472539+6604062630269@users.noreply.github.com>
2023-12-07 17:39:12 +0700

    Update HW_A.c

--- Week2/HW_A.c
+++ Week2/HW_A.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int a, b, c, d, m1, m2, m3, m4;
+	int a, b, c, d, max1, max2;
 	scanf("%d %d %d %d", &a, &b, &c, &d);
-	m1 = a > b ? a : b;
-	m2 = m1 > c ? m1 : c;
-	m3 = m2 < d ? m2 : d;
 	
-	printf("max number is %d ",m3);
+	max1 = a > b ? a : b;
+	max1 = max1 > c ? max1 : c;
+	max1 = max1 > d ? max1 : d;
+	
+	max2 = a == max1 ? b : a;
+	max2 = max1 > c && c > max2 ? c : max2;
+	max2 = max1 > d && d > max2 ? d : max2;
+	
+	
+	printf("max number 2nd is %d ",max2);
 }
