80a18d67581c5457481ab1009e372e804a2ce689
6604062630269 <152472539+6604062630269@users.noreply.github.com>
2023-12-07 15:01:01 +0700

    Add files via upload

---
+++ Week2/Week2/Ex_2.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	char X = 'A', y = 'B';
+	printf("5d-ANT \n%c-Bird\n", X, Y)
+}
---
+++ Week2/Week2/Ex_4.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	int a,b,c;
+	printf("Enter three integer number: ");
+	scanf("%d    %d %d", &a, &b, &c);
+	printf("a = %d b = %d c = %d\n", a, b, c);
+}
