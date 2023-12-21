dec88aa55a5392074f0c75ddacd1ceec95b790c4
6604062630269 <152472539+6604062630269@users.noreply.github.com>
2023-12-21 13:32:40 +0700

    ko

--- Week3/Ex_1.c
+++ Week3/Ex_1.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main(void) {
+    int score;
+    scan("%d", &score);
+    if (score >= 40) {
+        printf("Congratulations! You ");
+        printf("See you next semeter\n");
+    }
+}
