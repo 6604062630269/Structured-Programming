8ea126652e49e98d75f9eed8ae91688eca25f6f5
6604062630269 <s6604062630269@email.kmutnb.ac.th>
2023-12-21 14:00:38 +0700

    awd

--- Week3/Ex_1.c
+++ Week3/Ex_1.c
@@ -6,6 +6,10 @@ int main(void) {
     if (score >= 40) {
         printf("Congratulations! You ");
         printf("See you next semeter\n");
+    } else if (score >= 30) {
+        printf("You Can Test Again\n")
+        scanf("%d", &score);
+        if ()
     } else {
         printf("You are false \n");
         printf("See you again\n");
