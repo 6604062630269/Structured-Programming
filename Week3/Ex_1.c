868975d566b0b5d19667ab3b68db1a6733653644
6604062630269 <152472539+6604062630269@users.noreply.github.com>
2023-12-21 06:35:57 +0000

    wad

--- Week3/Ex_1.c
+++ Week3/Ex_1.c
@@ -6,5 +6,8 @@ int main(void) {
     if (score >= 40) {
         printf("Congratulations! You ");
         printf("See you next semeter\n");
+    } else {
+        printf("You are false \n");
+        printf("See you again\n");
     }
 }
