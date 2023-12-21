97d22ad6e7431bd42749f2531adaf354296a16f8
6604062630269 <s6604062630269@email.kmutnb.ac.th>
2023-12-21 14:24:10 +0700

    wad

--- Week3/Ex_5.c
+++ Week3/Ex_5.c
@@ -3,15 +3,15 @@
 int main(void) {
     int month;
     scanf("%d", &month);
-    month = month % 12
+    month = month % 12;
     if (month <= 2) {
         printf("Winter\n");
     } else if (month <= 5) {
         printf("Spring\n");
-    } else if (month <= 8)
+    } else if (month <= 8) {
         printf("Summer\n");
-        printf("Winter in %d month", 12-month)
-    } else if (month <= 11)
+        printf("Winter in %d month", 12-month);
+    } else if (month <= 11) {
         printf("Autumn\n");
     }
 }
