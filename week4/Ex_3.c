d6bac877561fca295171df88377f33249a36264b
6604062630269 <152472539+6604062630269@users.noreply.github.com>
2023-12-28 15:20:23 +0700

    Update Ex_3.c

--- week4/Ex_3.c
+++ week4/Ex_3.c
@@ -4,9 +4,10 @@ int main (void){
 	int i = 1;
 	while (i <= 3) {
        	printf ("Row %d: ", i);
-       	for (int j = 1; j<= 5; j++)
-            printf ("%3d", j);
-       	printf ("\n");
+       	for (int j = 1; j<= 5; j++) {
+            printf("%3d", j);
+		}
+       	printf("\n");
        	i++;
 	}
    	return 0;
